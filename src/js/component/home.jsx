506e62bce33707cde0ecc9a6fc03e77583e0a7d7
julitar <126813769+julitar@users.noreply.github.com>
2023-05-03 19:26:33 +0000

    Navbar corregir márgenes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			
+			<Navbar />
 		</div>
 	);
 };
