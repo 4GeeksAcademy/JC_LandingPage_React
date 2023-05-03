6d594963b8cc75537808181f265a38ba88ea1968
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-03 19:03:05 +0000

    card a medias

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import Card from "./card";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			
+			<Card/>
 		</div>
 	);
 };
