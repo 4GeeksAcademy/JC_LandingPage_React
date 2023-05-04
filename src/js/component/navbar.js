c3b181d94acc3404d2252cd77e946ab7e569a8d0
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-04 18:02:28 +0000

    Version final, todo ok

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,29 +4,26 @@ const Navbar = () => {
     return (
         <div>
             <div className="row">
-                <div className="col-12">
-                <nav className="navbar navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand mx-3" href="#">My First REACT</a>
-
-                    <ul className="navbar-nav d-flex flex-row justify-content-end">
-                            <li className="nav-item mx-3">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item mx-3">
-                            <a className="nav-link" href="#">About</a>
-                            </li>
-                            <li className="nav-item mx-3">
-                            <a className="nav-link" href="#">Services</a>
-                            </li>
-                            <li className="nav-item mx-3">
-                            <a className="nav-link" href="#">Contact</a>
-                            </li>
-                    </ul>
-
-                    
-                </div>
-                </nav>
+                <div className="col-md-12">
+                    <nav className="navbar navbar-dark bg-dark">
+                        <div className="container-fluid m-auto w-75 overflow-auto">
+                            <a className="navbar-brand mx-3" href="#">My First REACT</a>
+                            <ul className="navbar-nav d-flex flex-row justify-content-end">
+                                <li className="nav-item mx-3">
+                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li className="nav-item mx-3">
+                                    <a className="nav-link" href="#">About</a>
+                                </li>
+                                <li className="nav-item mx-3">
+                                    <a className="nav-link" href="#">Services</a>
+                                </li>
+                                <li className="nav-item mx-3">
+                                    <a className="nav-link" href="#">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
             </div>
 		</div>
