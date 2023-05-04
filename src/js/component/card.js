f2a6c61a14183864ce5526966bbd47a2163d2a6a
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-05-04 16:38:54 +0000

    cards y footer done

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,7 +2,6 @@ import React from "react";
 
 function Card () {
 	return (
-        <div>
 			<div className="col-md-3">
 				<div className="card text-center">
 					<img src="https://media.istockphoto.com/vectors/default-image-icon-vector-missing-picture-page-for-website-design-or-vector-id1357365823?b=1&k=20&m=1357365823&s=170667a&w=0&h=y6ufWZhEt3vYWetga7F33Unbfta2oQXCZLUsEa67ydM=" className="card-img-top card-image" alt="..."/>
@@ -12,8 +11,7 @@ function Card () {
 						<a href="#" className="btn btn-primary">Find Out More!</a>
 					</div>
 				</div>
-			</div>
-		</div>
+			</div> 
     );
 };
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,17 +3,21 @@ import React from "react";
 //include images into your bundle
 
 import Card from "./card";
+import Footer from "./footer";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<div className="row" id="card-row">
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
+		<div className="text-center ">
+			<div className="m-auto w-75 overflow-auto">
+				<div className="row" id="card-row">
+					<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+				</div>
 			</div>
+			<Footer/>
 		</div>
 	);
 };
